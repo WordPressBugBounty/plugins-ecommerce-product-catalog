e187b98e1cdb8e223fddf93a483ceb3a5784e8b3
Sean Murphy <sean@iamseanmurphy.com>
2025-09-17 08:20:38 +0000

    3.4.8

--- ecommerce-product-catalog/templates/template-parts/product-page/product-size.php
+++ ecommerce-product-catalog/templates/template-parts/product-page/product-size.php
@@ -1,6 +1,6 @@
 <?php
-if ( !defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
 }
 
 /**
@@ -8,21 +8,21 @@ if ( !defined( 'ABSPATH' ) ) {
  *
  * Copy it to your theme implecode folder to edit the output: your-theme-folder-name/implecode/product-size.php
  *
- * @version		1.1.2
- * @package		ecommerce-product-catalog/templates/template-parts/product-page
- * @author 		impleCode
+ * @version        1.1.2
+ * @package        ecommerce-product-catalog/templates/template-parts/product-page
+ * @author        impleCode
  */
-$product_id	 = ic_get_product_id();
-$size		 = ic_get_product_size( $product_id );
-if ( is_ic_attributes_size_enabled() && !empty( $size ) ) {
-	?>
+$product_id = ic_get_product_id();
+$size       = ic_get_product_size( $product_id, true );
+if ( is_ic_attributes_size_enabled() && ! empty( $size ) ) {
+    ?>
 
-	<table class="size-table">
-		<tr>
-			<td><?php echo ic_attributes_get_size_label() ?>:</td>
-			<td class="size-value"><?php echo $size ?></td>
-		</tr>
-	</table>
+    <table class="size-table">
+        <tr>
+            <td><?php echo ic_attributes_get_size_label() ?>:</td>
+            <td class="size-value"><?php echo $size ?></td>
+        </tr>
+    </table>
 
-	<?php
+    <?php
 }
